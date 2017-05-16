3bbfc624812c2769b4cb7c1c7dd716e53640049e
daniel-e <git.daniele@gmail.com>
2017-05-16 23:07:26 +0200

    initial version with hyper

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,31 @@
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 extern crate env_logger;
-extern crate getopts;
-// see issue: https://github.com/rust-lang-nursery/rustc-serialize/issues/61
-extern crate rustc_serialize;
+extern crate hyper;
 
-extern crate redis;
-#[macro_use] extern crate rustful;
-extern crate captcha;
+use hyper::server::{Server, Request, Response};
+use hyper::status::StatusCode;
+use hyper::header::ContentType;
+use hyper::mime::{Mime, TopLevel, SubLevel, Attr, Value};
 
-use std::error::Error;
+const PORT: u32 = 8080;
+const THREADS: usize = 8;
 
-use rustful::{Server, Context, Response, TreeRouter};
-use rustful::header::ContentType;
-
-mod arguments;
-mod config;
-mod rest_methods;
-mod executor;
-mod session;
-mod persistence;
-mod captchatools;
-
-use arguments::parse_arguments;
-use config::Config;
-use rest_methods::{req_create_captcha, req_get_catpcha, req_check_solution};
-use captcha::{init, done};
-
-// The code here is responsible for:
-// * reading command line arguments (via module arguments.rs)
-// * reading configuration file (via module config.rs)
-// * running the server, receiving requests and sending responses (via rustful)
-
-#[derive(Clone, Copy)]
-enum RequestType {
-    CreateCaptcha,
-    GetCaptcha,
-    CheckSolution,
-}
-
-struct Handler(Config, RequestType);
-
-impl rustful::Handler for Handler {
-
-    fn handle_request(&self, context: Context, mut response: Response) {
-
-        let config = self.0.clone();
-        let req_type = self.1;
-
-        let (body, status) = match req_type {
-            RequestType::CreateCaptcha => req_create_captcha(&mut response, config),
-            RequestType::GetCaptcha => req_get_catpcha(context, config),
-            RequestType::CheckSolution => req_check_solution(context, config),
-        };
-
-        response
-            .headers_mut()
-            .set(ContentType(content_type!(Application / Json; Charset = Utf8)));
-        response.set_status(status);
-        response.send(body);
-    }
+fn incoming(req: Request, mut res: Response) {
+    *res.status_mut() = StatusCode::BadRequest;
+    res.headers_mut().set(ContentType(
+            Mime(TopLevel::Application, SubLevel::Json, vec![(Attr::Charset, Value::Utf8)]))
+    );
+    res.send(b"hello");
 }
 
 fn main() {
-    env_logger::init().unwrap();
+    env_logger::init().expect("initializing logger failed");
 
-    let conf = match parse_arguments() {
-        Some(args) => {
-            match Config::parse_config(&args.config_file) {
-                Ok(conf) => conf,
-                Err(msg) => {
-                    error!("Could not read configuration file: {}", msg);
-                    return;
-                }
-            }
-        }
-        _ => { return; }
-    };
+    info!(target: "main", "Starting server on port {} ...", PORT);
 
-    info!(target: "main", "Starting server on port {} ...", conf.port);
-
-    init();
-
-    let srv = Server {
-        host: conf.port.into(),
-        handlers: insert_routes! {
-            TreeRouter::new() => {
-                "/session" => Post: Handler(conf.clone(), RequestType::CreateCaptcha),
-                "/session/:id" => Get: Handler(conf.clone(), RequestType::GetCaptcha),
-                "/session/:id" => Post: Handler(conf.clone(), RequestType::CheckSolution),
-            }
-        },
-        ..Server::default() // for the rest use default values
-    }.run();
-
-    done();
-
-    match srv {
-        Ok(_)  => { info!("Quit.") },
-        Err(e) => { error!("Could not start server: {}", e.description()) }
-    }
+    Server::http(format!("0.0.0.0:{}", PORT))
+        .expect("http failed")
+        .handle_threads(incoming, THREADS)
+        .expect("handle failed");
 }
